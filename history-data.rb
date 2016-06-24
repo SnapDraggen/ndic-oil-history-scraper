2e2a964789f241b45b2172a9f6ae196626e2dd8e
SnapDraggen <zmckenna321@gmail.com>
2016-06-23 22:37:54 -0600

    Added Support for Multiple Wells
    After the ruby command you can add multiple well id's separated by
    spaces to download multiple well files.

--- history-data.rb
+++ history-data.rb
@@ -7,35 +7,39 @@ require 'uri'
 require 'yaml'
 require 'openssl'
 time = Time.now.to_s.gsub!(':','.')
+x = 0;
+
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 config = YAML.load_file('auth.yml')
-
-uri = URI.parse("https://www.dmr.nd.gov/oilgas/feeservices/getwellprod.asp")
-request = Net::HTTP::Post.new(uri)
-request.content_type = "application/x-www-form-urlencoded"
-request["Authorization"] = config['auth']
-request.set_form_data(
-  "FileNumber" => "#{ARGV[0]}",
-  "B1" => "Get Monthly Production Data",
-)
-
-response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
-  http.request(request)
-end
-
-page = Nokogiri::HTML(response.body)
-
-
-rows = []
-page.css('table').last.css('tr').each do |tr|
-  rows << tr.css('td').map(&:content)
-end
-
-puts "Leeeeeroooy Jeenkins!"
-
-CSV.open("well number- #{ARGV[0]} time- "+ time +" .csv", 'w') do |file|
-  rows.each do |row|
-    file << row
-  end
+while ARGV[x] != nil do
+	uri = URI.parse("https://www.dmr.nd.gov/oilgas/feeservices/getwellprod.asp")
+	request = Net::HTTP::Post.new(uri)
+	request.content_type = "application/x-www-form-urlencoded"
+	request["Authorization"] = config['auth']
+	request.set_form_data(
+	  "FileNumber" => "#{ARGV[x]}",
+	  "B1" => "Get Monthly Production Data",
+	)
+	
+	response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
+	  http.request(request)
+	end
+	
+	page = Nokogiri::HTML(response.body)
+	
+	
+	rows = []
+	page.css('table').last.css('tr').each do |tr|
+	  rows << tr.css('td').map(&:content)
+	end
+	
+	puts "Leeeeeroooy Jeenkins!"
+	
+	CSV.open("well number- #{ARGV[x]} time- "+ time +" .csv", 'w') do |file|
+	  rows.each do |row|
+	    file << row
+	  end
+	end
+	x = x + 1;
 end
